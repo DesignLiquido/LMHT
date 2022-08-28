cb766ed51adf21e8232d7bb663065ae60664d20a
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-08-27 17:08:17 -0700

    Ajustando testes para Raspberry Pi

--- testes.py
+++ testes.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import lxml.etree as ET
 import glob
 import os
 
 xslt = ET.parse("lmht.xslt")
-for nome_arquivo in glob.glob(".\estruturas\*.lmht"):
+for nome_arquivo in glob.glob(os.path.join(os.getcwd(), "estruturas/*.lmht")):
     arquivo_lmht = ET.parse(nome_arquivo)
     transform = ET.XSLT(xslt)
     conteudo_html = transform(arquivo_lmht)
-    conteudo_html.write(f"{os.path.splitext(nome_arquivo)[0]}.html", encoding="utf-8")
+    nome_html = Path(nome_arquivo).stem + ".html"
+    caminho_html = os.path.join(os.getcwd(), nome_html)
+    conteudo_html.write(caminho_html, encoding="utf-8")
